c34858706d063fba79f7aca82d89fa3e03294f9f
Jack Linton <jacklinton@mac.com>
2017-02-02 11:47:28 -0500

    Added additional files to directory.

---
+++ exercizes2.1.17.rb
@@ -0,0 +1,23 @@
+name = "Jack Linton"
+
+p name
+
+holidays = ["Birthday", "Valentines Day", "Easter", "Memorial Day", "Independence Day", "Halloween", "Christmas"]
+
+p holidays
+
+holidays_and_dates = {
+
+	"Birthday" => "10/24/1982",
+	"Valentines_Day" => "2/14/2017",
+	"Easter" => "4/15/2017",
+	"Memorial_Day" => "9/1/2017",
+	"Independence_Day" => "7/4/2017",
+	"Halloween" => "10/31/2017",
+	"Chistmas" => "12/25/2017"
+
+}
+
+p holidays_and_dates
+
+p holidays_and_dates["Birthday"]
